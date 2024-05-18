ab1631d22299a16c041af7f3218c5fbe50288b35
Kenneth Grina <grina.kenneth@gmail.com>
2024-05-18 12:33:03 -0500

    drop down menu

--- src/components/FavMenu/FavMenu.jsx
+++ src/components/FavMenu/FavMenu.jsx
@@ -1,4 +1,4 @@
-//import OutlinedInput from '@mui/material/OutlinedInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -6,25 +6,32 @@ import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function FavMenu() {
     const [favCategory, setFavCategory] = useState([])
     const categories = useSelector(store => store.categories)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({ type: 'GET_CATEGORIES'})
+    }, [])
     
     return(
         <div>
-            <FormControl>
+            <p>Hello</p>
+            <FormControl sx={{ m: 1, width: 300 }}>
                 <InputLabel>Add Category</InputLabel>
                 <Select
                     multiple
                     value={favCategory}
                     renderValue={(selected) => selected.join(', ')}
+                    input={<OutlinedInput label="Tag" />}
                 >
                     {categories.map(cat => {
                         return (
                             <MenuItem key={cat.id} value={cat.name}>
-                                <Checkbox checked={favCategory.indexOf(cat.name) > -1} />
+                                <Checkbox checked={favCategory.indexOf(cat.id) > -1} />
                                 <ListItemText primary={cat.name} />
                             </MenuItem>
                         )
