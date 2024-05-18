19ae510b1a73d3b855c91b56686bf69d7d78d705
lons <lonsnw@gmail.com>
2024-05-18 08:40:10 -0500

    Adding Routes setup and Header component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "../Header/Header.jsx";
+
+
 function App() {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
+    <div className="app">
+      <Router>
+        <Header />
+          {/* <Route exact path="/" element={<Router_Name />} />
+          <Route path="/ROUTE" element={<Router_Name />} />
+          <Route path="/ROUTE" element={<Router_Name />} />
+          <Route path="/ROUTE" element={<Router_Name />} /> */}
+      </Router>      
     </div>
   );
 }
