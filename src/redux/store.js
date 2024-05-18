f3e9fc36105dd91c16eb98b05b7653242bfda26c
David Smith <djsmith611@me.com>
2024-05-18 09:17:37 -0700

    added redux route to delete favorite

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,15 +8,27 @@ import axios from 'axios';
 
 
 /**
+ * Function to delete favorites
+ */
+function* deleteFavorite(action) {
+    const id = action.payload.id;
+    try {
+        yield call(axios.delete(`/api/favorites/${id}`))
+        // refresh store     
+    } catch(error) {
+        console.log( "Error in deleteFavorite generator:", error);
+    }
+}
+
+/**
  * Function to add multiple categories
  */
-function* addRelations() {
+function* addRelations(action) {
     const payload = action.payload;
-    try{
+    try {
         yield call(axios.post("/api/categories",payload));
         // refresh store
-    }
-    catch(error){
+    } catch(error) {
         console.log("Error in addRelations generator:", error);
     }
 }
@@ -29,13 +41,14 @@ const sagaMiddleware = createSagaMiddleware();
 // Adding rootSaga to listen for saga actions
 function* rootSaga() {
     yield takeEvery ("ADD_CATEGORIES", addRelations);
+    yield takeEvery ("DELETE_FAVORITE", deleteFavorite);
 
 };
 
 // Creating a store for all of the components to use
 const storeInstance = createStore(
     combineReducers({
-        
+
     }),
     // Adding sagaMiddleware to the store
     applyMiddleware(sagaMiddleware, logger),
