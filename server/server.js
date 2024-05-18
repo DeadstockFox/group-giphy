0d9feb202ad57a6dff686f98266d9932d524b84c
lons <lonsnw@gmail.com>
2024-05-18 11:15:46 -0500

    Adjusting GET to be in store, adding reducers

--- server/server.js
+++ server/server.js
@@ -16,17 +16,6 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
-// Adding GET route to GIPHY API
-app.get('/api', (req, res) => {
-  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${search_term}&limit=1&offset=7&rating=g&lang=en&bundle=messaging_non_clips`)
-  .then((response) => {
-    console.log('Response from the Giphy API:', response.data);
-    res.send(response.data);
-  }).catch((error) => {
-    console.log('Error response from the Giphy API:', error);
-    res.sendStatus(500);
-  })
-})
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -12,24 +12,40 @@ import axios from 'axios';
  */
 
 // Add generator functions here
-
-
+function* fetchGif(action) {
+    try {
+        const searchTerm = action.payload;
+        console.log(searchTerm);
+        const giphyResponse = yield axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}&limit=1&offset=7&rating=g&lang=en&bundle=messaging_non_clips`);
+        yield put({ type: 'SET_GIF', payload: giphyResponse.data })
+    } catch (error) {
+        console.log('getGifs error', error);
+    }
+}
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 // Add saga actions here
-
+const gif = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_GIF':
+            return action.payload;
+        default:
+            return state;
+    }
+}
 
 // Adding rootSaga to listen for saga actions
 function* rootSaga() {
+    yield takeEvery('FETCH_GIF', fetchGif);
 
 };
 
 // Creating a store for all of the components to use
 const storeInstance = createStore(
     combineReducers({
-
+        gif,
 
     }),
     // Adding sagaMiddleware to the store
