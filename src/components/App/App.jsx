a327efc0d0d39dac88573bbedc0df6fe295da698
lons <lonsnw@gmail.com>
2024-05-18 11:23:26 -0500

    Adding components to build out landing page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "../Header/Header.jsx";
-
+import Home from "../Home/Home.jsx";
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Header />
-          {/* <Route exact path="/" element={<Router_Name />} />
-          <Route path="/ROUTE" element={<Router_Name />} />
+          {/* Home page with search and gif result */}
+          <Route exact path="/" element={<Home />} />
+          {/* <Route path="/ROUTE" element={<Router_Name />} />
           <Route path="/ROUTE" element={<Router_Name />} />
           <Route path="/ROUTE" element={<Router_Name />} /> */}
       </Router>      
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import SearchBar from "../SearchBar/SearchBar";
+import GifResult from "../GifResult/GifResult";
+
+function Home() {
+
+    return (
+        <main>
+            <SearchBar />
+            <GifResult />
+        </main>
+    )
+}
+
+export default Home;
