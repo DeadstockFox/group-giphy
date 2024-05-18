a8fbee4e3e23f38c99ac1cfbf1ab8ec2d63120be
David Smith <djsmith611@me.com>
2024-05-18 10:26:45 -0700

    added nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,28 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Link, Box, Typography } from '@mui/material';
+
 function Header() {
-
-
-    return(
-        <>
-        <h1>It's "Gif" not "Jif"</h1>
-        </>
-    )
+    return (
+        <Box 
+            display="flex" 
+            justifyContent="space-between" 
+            alignItems="center" 
+            padding={2}
+            sx={{ backgroundColor: 'lightgray' }}
+        >
+            <Typography variant="h4">
+                It's "Gif" not "Jif"
+            </Typography>
+            <Box>
+                <Link component={RouterLink} to="/" sx={{ marginRight: 2 }}>
+                    Home
+                </Link>
+                <Link component={RouterLink} to="/favorite">
+                    Favorite
+                </Link>
+            </Box>
+        </Box>
+    );
 }
 
 export default Header;
